cc625428154d80e12c8a25a10469206206557273
1N50MN14 <ayman.mackouly@gmail.com>
2013-02-27 15:49:46 +0100

    support for textContent, setAttribute overrides if exists, added getAttribute

---
+++ example.js
@@ -0,0 +1,5 @@
+require('./index.js')
+var h1=document.createElement('h1');
+h1.setAttribute('n', 'v');
+h1.textContent = 'w0f w0f';
+console.log(h1.toString());
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function Document() {}
 
 Document.prototype.createTextNode = function(v) {
     var n = new Text();
-    n.value = v;
+    n.textContent = v;
     return n;
 }
 
@@ -21,6 +21,7 @@ Document.prototype.createElement = function(nodeName) {
 function Node () {}
 
 Text.prototype = new Node()
+
 Element.prototype = new Node()
 
 function Style () {}
@@ -29,15 +30,38 @@ Style.prototype.setProperty = function (k,v) {
   this[k] = v
 }
 
-function Attribute(){}
+function Attribute(name, value){  
+  if (name) {
+    this.name = name.toLowerCase();
+    this.value = value ? value : '';
+  }  
+}
 
 
 function Element() {
+    var self = this;
+
     this.classList = []
     this.classList.add = this.classList.push.bind(this.classList);
-    this.style = new Style
+    this.style = new Style()
     this.childNodes = [];
     this.attributes = [];
+
+    this._setProperty = function(arr, Obj, key, val) {
+      var p = self._getProperty(arr, key);      
+      if (p) {
+        p.value = val;
+        return;
+      }
+      arr.push(new Obj(key,val));
+    }
+
+    this._getProperty = function(arr, key) {
+      key = key.toLowerCase();
+      for (var i=0;i<arr.length;i++) {
+        if (key == arr[i].name) return key[i];
+      }
+    }
 }
 
 Element.prototype.appendChild = function(child) {
@@ -45,11 +69,12 @@ Element.prototype.appendChild = function(child) {
     this.childNodes.push(child);
 }
 
-Element.prototype.setAttribute = function (k, v) {
-    var a = new Attribute
-    a.name = k
-    a.value = v
-    this.attributes.push(a)    
+Element.prototype.setAttribute = function (n, v) {
+    this._setProperty(this.attributes, Attribute, n, v);
+}
+
+Element.prototype.getAttribute = function(n) {
+    return this._getProperty(this.attributes, n);
 }
 
 Element.prototype.replaceChild = function(newChild, oldChild) {
@@ -66,12 +91,13 @@ Element.prototype.toString = function () {
   function _stringify(arr) {
     var attr = [];
     arr.forEach(function(a){
-      attr.push(a.name+'=\''+a.value+'\'');
+      attr.push(a.name+'=\"'+a.value+'\"');
     })
     return attr.length ? ' '+attr.join(" ") : '';
   }    
 
   a.push('<'+this.nodeName + _stringify(this.attributes)+'>')
+  this.textContent && a.push(this.textContent);
   this.childNodes.forEach(function (e) {
     a.push(e.toString())
   })
@@ -79,6 +105,8 @@ Element.prototype.toString = function () {
 
   return a.join('\n')
 }
+
+Element.prototype.addEventListener = function(t, l) {}
 
 function escapeHTML(s) {
   return String(s)
@@ -89,7 +117,8 @@ function escapeHTML(s) {
       .replace(/>/g, '&gt;');
   }
 
-function Text(){}
+function Text(){
+}
 
 Text.prototype.toString = function() {
     return escapeHTML(this.value);
