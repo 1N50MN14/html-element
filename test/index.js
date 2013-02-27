f6d3d5e8388d5f5fd890fec4cc1022a31aa4313b
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-28 10:18:54 +1300

    update tests to use outerHTML and textContent

--- test/index.js
+++ test/index.js
@@ -2,9 +2,27 @@ require('../');
 
 var test = require('tap').test;
 
+//remove white space so it's easier to test
+function clean(e) {
+  return e.replace(/\n/g, '')
+          .replace(/\s+/, ' ')
+}
+
 test('create a Text node', function(t){
-	var h1 = document.createElement('h1')
-	h1.setAttribute('class', 'myclass');			
-	t.type(h1.toString(), "string", "type of h1 should be string")
-	t.end();
+  var h1 = document.createElement('h1')
+  t.equal(clean(h1.outerHTML), "<h1></h1>")
+  t.equal(clean(h1.textContent), "")
+
+  h1.setAttribute('class', 'myclass');      
+  t.type(h1.toString(), "string", "type of h1 should be string")
+
+  t.equal(clean(h1.outerHTML), '<h1 class="myclass"></h1>')
+  t.equal(clean(h1.textContent), "")
+
+  h1.appendChild(document.createTextNode('hello'))
+
+  t.equal(clean(h1.outerHTML), '<h1 class="myclass">hello</h1>')
+  t.equal(clean(h1.textContent), "hello")
+  t.end();
 })
+
