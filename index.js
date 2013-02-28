fa4a419f094374953a384d6a498257a7d013402a
1N50MN14 <ayman.mackouly@gmail.com>
2013-02-28 17:44:42 +0100

    check for self.className.length instead #169

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function Element() {
     this.classList = new classList(this);
     this.childNodes = [];
     this.attributes = [];
+    this.className = '';
 
     this._setProperty = function(arr, obj, key, val) {
       var p = self._getProperty(arr, key);      
@@ -165,7 +166,7 @@ Element.prototype.__defineGetter__('outerHTML', function () {
     }    
     // special className case, if className property is define while 'class' attribute is not then
     // include class attribute in output
-    self.className && !self.getAttribute('class') && props.push({name:'class', value: self.className})   
+    self.className.length && !self.getAttribute('class') && props.push({name:'class', value: self.className})   
     return props ? _stringify(props) : '';
   }
 
