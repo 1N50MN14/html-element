accf47d2b6b189bf9e969171624ef1e5a7a638f4
t ddddddd <webdev@cmme.org>
2016-08-20 18:47:39 -0700

    1st half(ish) of attribute-element mappings

--- attributes-elements.js
+++ attributes-elements.js
@@ -10,11 +10,15 @@ var HTML_ATTRIBUTES = {
     'input',
   ]),
 
-  'accept-charset': 'form',
+  'accept-charset': new Set([
+    'form',
+  ]),
 
   'accesskey': 'GLOBAL',
 
-  'action': 'form',
+  'action': new Set([
+    'form',
+  ]),
 
   'align': new Set([
     'applet',
@@ -40,14 +44,214 @@ var HTML_ATTRIBUTES = {
     'input',
   ]),
 
-  'async': 'script',
+  'async': new Set([
+    'script',
+  ]),
 
   'autocomplete': new Set([
     'form',
     'input',
   ]),
 
-  // TODO
+  'autofocus': new Set([
+    'button',
+    'input',
+    'keygen',
+    'select',
+    'textarea',
+  ]),
+
+  'autoplay': new Set([
+    'audio',
+    'video',
+  ]),
+
+  'autosave': new Set([
+    'input',
+  ]),
+
+  'bgcolor': new Set([
+    'body',
+    'col',
+    'colgroup',
+    'marquee',
+    'table',
+    'tbody',
+    'tfoot',
+    'td',
+    'th',
+    'tr',
+  ]),
+
+  'border': new Set([
+    'img',
+    'object',
+    'table',
+  ]),
+
+  'buffered': new Set([
+    'audio',
+    'video',
+  ]),
+
+  'challenge': new Set([
+    'keygen',
+  ]),
+
+  'charset': new Set([
+    'meta',
+    'script',
+  ]),
+
+  'checked': new Set([
+    'command',
+    'input',
+  ]),
+
+  'cite': new Set([
+    'blockquote',
+    'del',
+    'ins',
+    'q',
+  ]),
+
+  'class': 'GLOBAL',
+
+  'code': new Set([
+    'applet',
+  ]),
+
+  'codebase': new Set([
+    'applet',
+  ]),
+
+  'color': new Set([
+    'basefont',
+    'font',
+    'hr',
+  ]),
+
+  'cols': new Set([
+    'textarea',
+  ]),
+
+  'colspan': new Set([
+    'td',
+    'th',
+  ]),
+
+  'content': new Set([
+    'meta',
+  ]),
+
+  'contenteditable': 'GLOBAL',
+
+  'contextmenu': 'GLOBAL',
+
+  'controls': new Set([
+    'audio',
+    'video',
+  ]),
+
+  'coords': new Set([
+    'area',
+  ]),
+
+  'data': new Set([
+    'object',
+  ]),
+
+  'datetime': new Set([
+    'del',
+    'ins',
+    'time',
+  ]),
+
+  'default': new Set([
+    'track',
+  ]),
+
+  'defer': new Set([
+    'script',
+  ]),
+
+  'dir': 'GLOBAL',
+
+  'dirname': new Set([
+    'input',
+    'textarea',
+  ]),
+
+  'disabled': new Set([
+    'button',
+    'command',
+    'fieldset',
+    'input',
+    'keygen',
+    'optgroup',
+    'option',
+    'select',
+    'textarea',
+  ]),
+
+  'download': new Set([
+    'a',
+    'area',
+  ]),
+
+  'draggable': 'GLOBAL',
+
+  'dropzone': 'GLOBAL',
+
+  'enctype': new Set([
+    'form',
+  ]),
+
+  // FIXME translate from htmlFor
+  'for': new Set([
+    'label',
+    'output',
+  ]),
+
+  'form': new Set([
+    'button',
+    'fieldset',
+    'input',
+    'keygen',
+    'label',
+    'meter',
+    'object',
+    'output',
+    'progress',
+    'select',
+    'textarea',
+  ]),
+
+  'formaction': new Set([
+    'input',
+    'button',
+  ]),
+
+  'headers': new Set([
+    'td',
+    'th',
+  ]),
+
+  'height': new Set([
+    'canvas',
+    'embed',
+    'iframe',
+    'img',
+    'input',
+    'object',
+    'video',
+  ]),
+
+  'hidden': 'GLOBAL',
+
+  'high': new Set([
+    'meter',
+  ]),
 
   'href': new Set([
     'a',
@@ -64,7 +268,6 @@ function isStandardAttribute(attrName, tagName) {
   var attr = HTML_ATTRIBUTES[attrName.toLowerCase()];
   return !!attr && (
     attr === 'GLOBAL' ||
-    attr === tagName ||
     attr.has(tagName)
   );
 }
